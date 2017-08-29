e4e305e1dfab334fe84491cb30311f9cfac1b949
woodcrust <roboucrop@gmail.com>
2017-08-29 06:28:48 +0300

    fix

--- resources/users.rb
+++ resources/users.rb
@@ -8,17 +8,32 @@ property :users,    Array,  required: true
 property :rvm_path, String, required: true
 
 action :install do
-  break if rvm_exist?
+  break unless rvm_exist?
 
   users.each do |user|
     next if belong_to_rvm_group?(user)
 
     ruby_block "export rvm_path #{rvm_path} to /etc/profile" do
-      rvm_export_path        = "export rvm_path=#{rvm_path}"
+      rvm_export_path        = %(export rvm_path=#{rvm_path}\nexport PATH=$PATH:#{rvm_path}/rubies/default/bin)
       rvm_export_path_regexp = /rvm/
 
       block do
         file = Chef::Util::FileEdit.new('/etc/profile')
+        file.insert_line_if_no_match(rvm_export_path_regexp, "\n#{rvm_export_path}")
+        file.write_file
+      end
+    end
+
+    ruby_block "export rvm env to user profile" do
+      rvm_export_path        = %(export PATH="$PATH:#{rvm_path}/bin"\n[[ -s "#{rvm_path}/scripts/rvm" ]] && source "#{rvm_path}/scripts/rvm"\nexport PATH=$PATH:#{rvm_path}/rubies/default/bin)
+      rvm_export_path_regexp = /#{rvm_path}\/scripts\/rvm/
+
+      block do
+        file = Chef::Util::FileEdit.new("/home/#{user}/.profile")
+        file.insert_line_if_no_match(rvm_export_path_regexp, "\n#{rvm_export_path}")
+        file.write_file
+
+        file = Chef::Util::FileEdit.new("/home/#{user}/.bashrc")
         file.insert_line_if_no_match(rvm_export_path_regexp, "\n#{rvm_export_path}")
         file.write_file
       end
